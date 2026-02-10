2b6e5c5694cc3c0ffcfb5b5def8491874428490a
Md. Rashedul Islam <santorasu09@gmail.com>
2026-02-11 00:08:18 +0600

    this is my first nest js practice code

---
+++ src/user/user.controller.ts
@@ -0,0 +1,9 @@
+import { Controller ,Get} from '@nestjs/common';
+
+@Controller('user') // This decorator marks the class as a NestJS controller and specifies the route prefix for all routes in this controller.
+export class UserController {
+@Get()
+getUser(){
+    return 'User data fetched successfully...';
+}
+}
